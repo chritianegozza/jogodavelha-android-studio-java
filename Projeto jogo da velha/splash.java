34dce65a1ef6aaab522bfd035896a2ef1aaf8481
christiane gozza <72118415+chritianegozza@users.noreply.github.com>
2021-06-06 13:41:17 -0300

    Add files via upload

---
+++ Projeto jogo da velha/splash.java
@@ -0,0 +1,35 @@
+splash.java
+
+
+
+package com.example.jogodavelha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash);
+        getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        },2500);
+    }
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(Splash.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
